244a18ed862e9407666d711109f11871112263d7
fy <305120283@qq.com>
2015-05-04 20:44:42 -0400

    return face and palm images
    changed return value of the function

--- detectFace.m
+++ detectFace.m
@@ -1,4 +1,4 @@
-function faceSet = detectFace( input_img )
+function [face, palm] = detectFace( input_img )
 %To detect Face
 FDetect = vision.CascadeObjectDetector;
 
@@ -13,13 +13,21 @@ faceSet = cell(size(BB,1));
 
 figure,
 imshow(I); hold on
+
+face_edge = 0;
+
 for i = 1:size(BB,1)
     rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
     up_left_c = BB(i,1); col_span = BB(i,3);
     up_left_r = BB(i,2); row_span = BB(i,4);
     facePic = I(up_left_r:up_left_r+row_span-1,up_left_c:up_left_c+col_span-1,:);
+    if i==size(BB,1)
+        face_edge = up_left_c+col_span-1;
+    end
     faceSet{i} = facePic;
 end
+face = faceSet{size(faceSet)};
+palm = I(:,face_edge:size(I,2),:);
 title('Face Detection');
 hold off;
 end
